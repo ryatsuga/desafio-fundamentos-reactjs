2e8f1936cee3850ac0e1420c943d12a4b6b44207
Leandro Passos <31132724+ryatsuga@users.noreply.github.com>
2020-09-05 14:06:17 -0300

    Primeira tentativa

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,19 @@
-const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
+function formatValue (value: number, type?: 'income' | 'outcome'): string {
+
+const valueCurrency = Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(value);
+
+if (type==='outcome') {
+  return '- ' + valueCurrency;
+}
+if (type==='income') {
+  return valueCurrency;
+}
+
+return valueCurrency;
+
+}
 
 export default formatValue;
